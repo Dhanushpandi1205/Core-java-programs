403e6d97fc0eba63144e0067f343048797a723a5
Dhanushpandi <dhanushpandijs@gmail.com>
2025-06-12 12:22:54 +0530

    Update

--- C2TC_Core_Java_Programs/src/day1/Addition.java
+++ C2TC_Core_Java_Programs/src/day1/Addition.java
@@ -5,9 +5,15 @@ public class Addition {
 
 	public static void main(String[] args) {
 		int a,b,c;
+		Scanner sc;
+		sc = new Scanner(System.in);
 		
-		a= Integer.parseInt(args[0]);
-		b= Integer.parseInt(args[0]);
+		System.out.println(" enter a: ");
+		a=sc.nextInt();
+		
+		System.out.println(" enter a: ");
+		b=sc.nextInt();
+		
 		c=a+b ;
 				
 				System.out.println("Addition of a="+a+"and b ="+b+"is"+c);
