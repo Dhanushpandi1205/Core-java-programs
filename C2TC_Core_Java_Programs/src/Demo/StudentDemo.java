af94ad677fd573e806513e74f55ee6943472781d
Deepak R <dpak24dpak@gmail.com>
2025-06-14 11:03:24 +0530

    update

---
+++ C2TC_Core_Java_Programs/src/Demo/StudentDemo.java
@@ -0,0 +1,22 @@
+package Demo;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+        st.setId(1060);
+        st.setName("Dhanushpandi");
+        st.setAge(20);
+       
+        System.out.println(st);
+        
+        
+        Student st1 = new Student();
+        st1.setId(6015);
+        st1.setName("Dhanush");
+        st1.setAge(19);
+        
+        System.out.println(st1);
+	}
+
+}
