ce46591a34f53def4b263ec9c512b9195b98f908
Dhanushpandi <dhanuhshpandijs@gmail.com>
2025-06-16 15:04:58 +0530

    scanner

---
+++ C2TC_Core_Java_Programs/src/scanner/ScannerDemo.java
@@ -0,0 +1,18 @@
+package scanner;
+import java.util.Scanner;
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		String name = scanner.nextLine();
+		
+		System.out.println("Enter your age :");
+		int age = scanner.nextInt();
+		
+		System.out.println(name+ " is "+ age);
+	}
+
+}
