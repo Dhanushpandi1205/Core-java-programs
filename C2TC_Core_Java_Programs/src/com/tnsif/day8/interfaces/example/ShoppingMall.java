74948002929136bced3f31e89257dcdabd94c29b
Dhanushpandi <dhanuhshpandijs@gmail.com>
2025-06-23 15:32:05 +0530

    This contains core java program

---
+++ C2TC_Core_Java_Programs/src/com/tnsif/day8/interfaces/example/ShoppingMall.java
@@ -0,0 +1,11 @@
+package com.tnsif.day8.interfaces.example;
+
+public class ShoppingMall {
+
+	public static void main(String[] args) {
+	Purchase purchase = new Grocery();
+	purchase.buying();
+
+	}
+
+}
